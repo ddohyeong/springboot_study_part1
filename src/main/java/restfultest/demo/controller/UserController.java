d41b1436b8883775231233b8340c8caf243156ef
ddohyeong <dohyeong7026@gmail.com>
2024-03-18 21:21:14 +0900

    ResponseEntity 반환

--- src/main/java/restfultest/demo/controller/UserController.java
+++ src/main/java/restfultest/demo/controller/UserController.java
@@ -1,12 +1,15 @@
 package restfultest.demo.controller;
 
+import java.net.URI;
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
@@ -33,9 +36,21 @@ public class UserController {
 		return userDaoService.findOne(id);
 	}
 
-	@PostMapping(path = "/users")
-	public void createUser(@RequestBody User user){
-		System.out.println("user = " + user);
+	// @PostMapping(path = "/users")
+	// public void createUser(@RequestBody User user){
+	// 	System.out.println("user = " + user);
+	// 	User savedUser = userDaoService.save(user);
+	// }
+
+	@PostMapping("/users")
+	public ResponseEntity<User> createUser1(@RequestBody User user) {
 		User savedUser = userDaoService.save(user);
+
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}")
+				.buildAndExpand(savedUser.getId())
+				.toUri();
+		System.out.println("location = " + location);
+		return ResponseEntity.created(location).build();
 	}
 }
